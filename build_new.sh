602dbd383787ae65000ebc6458fd316fdfe6920e
Kontio Pyry <pyry.kontio@drasa.eu>
2017-08-24 20:06:17 +0900

    Simple demo.

---
+++ build_new.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker run -it --rm \
+    -v $PWD:/workdir \
+    -v ~/.cargo/git:/root/.cargo/git \
+    -v ~/.cargo/registry:/root/.cargo/registry \
+    golddranks/rust_musl_docker:nightly-2017-08-24 \
+    cargo build -vv --target=x86_64-unknown-linux-musl
