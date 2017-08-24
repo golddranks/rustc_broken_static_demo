70afb1c4d6c9c4fa7e2ab0301134f8cb5a417422
Kontio Pyry <kontio@r.recruit.co.jp>
2017-08-24 20:47:28 +0900

    Use the last working nightly 2017-08-23 instead of 08-21.

--- build_old.sh
+++ build_old.sh
@@ -3,5 +3,5 @@ docker run -it --rm \
     -v $PWD:/workdir \
     -v ~/.cargo/git:/root/.cargo/git \
     -v ~/.cargo/registry:/root/.cargo/registry \
-    golddranks/rust_musl_docker:nightly-2017-08-21 \
+    golddranks/rust_musl_docker:nightly-2017-08-23 \
     cargo build -vv --target=x86_64-unknown-linux-musl
